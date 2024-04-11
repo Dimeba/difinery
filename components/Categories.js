d5a97352c10a5b69fbbc75414a256fb27ad9c52b
Filip Stojanovic <philsty92@gmail.com>
2024-04-11 06:18:45 -0400

    Added categories.

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,36 @@
 // styles
 import styles from './Categories.module.scss'
 
+// components
+import Image from 'next/image'
+import Link from 'next/link'
+
 const Categories = () => {
+	const categories = ['bracellets', 'earrings', 'rings', 'pendants']
+
 	return (
 		<section>
-			<div className='container'>Categories</div>
+			<div className={`container ${styles.content}`}>
+				<h3>Shop by Category</h3>
+
+				<div className={styles.categories}>
+					{categories.map((item, index) => (
+						<div key={index} className={styles.categorie}>
+							<Link href='#' aria-label={`Link to ${item} page.`}>
+								<div className={styles.image}>
+									<Image
+										src={`/${item}.jpg`}
+										fill
+										alt='Category Image.'
+										style={{ objectFit: 'cover' }}
+									/>
+								</div>
+							</Link>
+							<h4>{item}</h4>
+						</div>
+					))}
+				</div>
+			</div>
 		</section>
 	)
 }
