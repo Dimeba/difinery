1dcaf73c572dffca276f8a1affcf7cb9e71482d3
Filip Stojanovic <philsty92@gmail.com>
2024-05-23 17:02:35 -0400

    Fixed favicon/

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
+			<head>
+				<link rel='icon' href='/favicon.svg' type='image/svg+xml' />
+			</head>
 			<body className={workSans.className}>
 				<Header />
 				{children}
