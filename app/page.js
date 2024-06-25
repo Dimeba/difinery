aa1fa774f4832335e22b7ea2c5a2d32ae372b886
Filip Stojanovic <philsty92@gmail.com>
2024-06-25 12:59:46 -0400

    Added Contentful to lib and banner sections to homepage.

--- app/page.js
+++ app/page.js
@@ -6,24 +6,14 @@ import Products from '@/components/Products'
 
 // lib
 import { getCollections } from '@/lib/shopify'
-
-// contentful
-import { createClient } from 'contentful'
+import { getEntries } from '@/lib/contentful'
 
 export default async function Home() {
 	// Shopify
 	const collections = await getCollections()
 
 	// Contentful
-	const client = createClient({
-		space: process.env.space,
-		accessToken: process.env.accessToken
-	})
-
-	const homepage = await client.getEntries({
-		content_type: 'homepage'
-	})
-
+	const homepage = await getEntries('homepage')
 	const content = homepage.items[0].fields
 
 	return (
@@ -33,36 +23,26 @@ export default async function Home() {
 				text={content.heroText}
 				image={content.heroImage.fields.file.url}
 			/>
-			<Banner
-				button1Text='Learn More'
-				button1Url='#'
-				button2Text='Shop Now'
-				button2Url='/shop'
-				video
-				center
-				url='/sample-video.mp4'
-				title='Sustainable Luxury'
-				text='We hold ourselves accountable for continuously minimizing our environmental footprint while providing premium quality jewelry that redefines forever.'
-			/>
-			<Banner
-				button1Text='Learn More'
-				button1Url='#'
-				image
-				center
-				url='/sample-image.jpg'
-				title='Ethical. Elegant. Ethereal'
-			/>
-			<Banner
-				button1Text='Learn More'
-				button1Url='#'
-				button2Text='Shop Now'
-				button2Url='/shop'
-				video
-				center
-				url='/sample-video.mp4'
-				title='Find timeless elegance in every piece.'
-				text='Sustainable and ethically-crafted fine jewelry is our essence. We are committed to fair value and a fair future. To us, forever is not just about every piece of jewelry lasting forever, but contributing to a forever future for our  planet, and its people.'
-			/>
+			{content.sections.map((section, index) => {
+				switch (section.sys.contentType.sys.id) {
+					case 'banner':
+						return (
+							<Banner
+								key={index}
+								title={section.fields.title}
+								text={section.fields.text}
+								links={section.fields.links}
+								image={section.fields.image}
+								video={section.fields.video}
+								showControls={section.fields.showVideoControls}
+							/>
+						)
+
+					default:
+						return null
+				}
+			})}
+
 			<Features />
 			<Products title='Shop by Category' categories={collections} />
 		</main>
--- components/Banner.js
+++ components/Banner.js
@@ -1,107 +1,59 @@
-'use client'
-
 // styles
 import styles from './Banner.module.scss'
-
-// hooks
-import { useIntersectionObserver } from '@/hooks/useIntersectionObserver'
-import { useState } from 'react'
 
 // components
 import Button from './Button'
 import Image from 'next/image'
-import {
-	IoPlaySharp,
-	IoPauseSharp,
-	IoVolumeMuteSharp,
-	IoVolumeHighSharp
-} from 'react-icons/io5'
+import Video from './Video'
 
-const Banner = ({
-	center,
+// lib
+import { getEntry } from '@/lib/contentful'
+
+const Banner = async ({
 	video,
 	image,
 	title,
 	text,
-	url,
-	button1Text,
-	button1Url,
-	button2Text,
-	button2Url,
+	links,
 	top,
 	showControls
 }) => {
-	const options = {
-		root: null,
-		rootMargin: '0%',
-		threshold: 0
+	const getLink = async id => {
+		const link = await getEntry(id)
+		return link
 	}
 
-	const [targetRef, isIntersecting] = useIntersectionObserver(options)
+	const buttons = []
 
-	const [isPlaying, setIsPlaying] = useState(true)
-	const [isMuted, setIsMuted] = useState(false)
-
-	const togglePlay = () => {
-		if (isPlaying) {
-			targetRef.current.pause()
-			setIsPlaying(false)
-		} else {
-			targetRef.current.play()
-			setIsPlaying(true)
+	if (links) {
+		for (let i = 0; i < links.length; i++) {
+			const link = await getLink(links[i].sys.id)
+			buttons.push(link)
 		}
-	}
-
-	const toggleMute = () => {
-		targetRef.current.muted = !targetRef.current.muted
-		setIsMuted(!targetRef.current.muted)
 	}
 
 	return (
 		<section className={`${styles.banner} ${top ? styles.top : ''}`}>
-			{video && (
-				<video ref={targetRef} autoPlay loop muted playsInline preload='auto'>
-					{isIntersecting && <source src={url} type='video/mp4' />}
-				</video>
+			{video && <Video video={video} showControls={showControls} />}
+
+			{image && (
+				<Image src={'https:' + image.fields.file.url} alt='image' fill />
 			)}
 
-			{image && <Image src={url} alt='image' fill />}
-
-			<div
-				className={`container ${styles.content} ${center ? styles.center : ''}`}
-			>
+			<div className={`container ${styles.content}`}>
 				{title && <h2>{title}</h2>}
 				{text && <p>{text}</p>}
-				{button1Text && (
+				{links && (
 					<div className={styles.buttons}>
-						{button1Text && (
-							<Button text={button1Text} link={button1Url} white />
-						)}
-						{button2Text && (
-							<Button text={button2Text} link={button2Url} white />
-						)}
+						{buttons.map((button, index) => (
+							<Button
+								key={index}
+								text={button.fields.text}
+								link={button.fields.url}
+								white
+							/>
+						))}
 					</div>
-				)}
-
-				{/* Video Controls */}
-				{video && showControls && (
-					<>
-						<div className={styles.playIcon} onClick={togglePlay}>
-							{isPlaying ? (
-								<IoPauseSharp color='white' size={26} />
-							) : (
-								<IoPlaySharp color='white' size={26} />
-							)}
-						</div>
-
-						<div className={styles.muteIcon} onClick={toggleMute}>
-							{isMuted ? (
-								<IoVolumeMuteSharp color='white' size={26} />
-							) : (
-								<IoVolumeHighSharp color='white' size={26} />
-							)}
-						</div>
-					</>
 				)}
 			</div>
 		</section>
