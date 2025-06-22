52c5f98b43ce53bd0fa2ab50b6f5a62fef031b81
Dimeba <philsty92@gmail.com>
2025-06-22 23:06:35 +0200

    Minor update.

--- components/MasonryColumns.js
+++ components/MasonryColumns.js
@@ -6,21 +6,13 @@ import Image from 'next/image'
 const MasonryColumns = ({ content }) => {
 	return (
 		<Grid container spacing={0}>
-			<Grid item size={{ xs: 12, sm: 6 }}>
-				<Column id={content[0].sys.id} columns={content.length} height='100%' />
+			<Grid size={{ xs: 12, sm: 6 }}>
+				<Column id={content[0].sys.id} columns={1} height='100%' />
 			</Grid>
-			<Grid item size={{ xs: 12, sm: 6 }}>
+			<Grid size={{ xs: 12, sm: 6 }}>
 				<Stack sx={{ width: '100%' }}>
-					<Column
-						id={content[1].sys.id}
-						columns={content.length}
-						height='100%'
-					/>
-					<Column
-						id={content[2].sys.id}
-						columns={content.length}
-						height='100%'
-					/>
+					<Column id={content[1].sys.id} columns={1} height='100%' />
+					<Column id={content[2].sys.id} columns={1} height='100%' />
 				</Stack>
 			</Grid>
 		</Grid>
