e6dcdf87353d59f7de390766e297a9f80a304e7f
Filip Stojanovic <philsty92@gmail.com>
2025-08-06 22:05:22 +0200

    Added other collections and queries.

---
+++ app/shop/collections/summer-selection/page.js
@@ -0,0 +1,41 @@
+// components
+import Products from '@/components/Products'
+import PageContent from '@/components/PageContent'
+
+// lib
+import { apolloClient } from '@/lib/apolloClient'
+import { GET_COLLECTION_BY_HANDLE } from '@/lib/queries/getCollectionByHandle'
+import { getEntries } from '@/lib/contentful'
+
+// Contentful
+const pages = await getEntries('page')
+const content = pages.items.find(page => page.fields.title == 'Shop').fields
+
+export const metadata = {
+	title: 'Difinery | Heart Collection',
+	description: '',
+	keywords: ''
+}
+
+export default async function Home() {
+	const { data } = await apolloClient.query({
+		query: GET_COLLECTION_BY_HANDLE,
+		variables: { handle: 'summer-collection', first: 16, after: null }
+	})
+
+	const initialEdges = data.collectionByHandle?.products.edges
+	const initialItems = initialEdges.map(edge => edge.node)
+	const initialPageInfo = data.collectionByHandle?.products.pageInfo
+
+	return (
+		<main>
+			<Products
+				products={initialItems}
+				initialPageInfo={initialPageInfo}
+				productType='all'
+				showFilters
+			/>
+			<PageContent content={content} />
+		</main>
+	)
+}
